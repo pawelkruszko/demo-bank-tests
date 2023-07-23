f9194249bf3b3f6675f2aa4d070803c4ac57e44f
Paweł Kruszko <pawelkruszko@MacBook-Pro-Pawe.local>
2023-07-23 10:45:27 +0200

    AAA tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,13 +1,22 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
+
   test('successful login with correct credentials', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('testerLO');
-    await page.getByTestId('password-input').fill('10987654');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = '10987654';
+    const expectedUserName = 'Jan Demobankowy';
+    
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
+    // Assert
+    await expect(page.getByTestId('user-name')).toHaveText(expectedUserName);
   });
 
   test('unsuccessful login with too short username', async ({ page }) => {
--- tests/plupit.spec.ts
+++ tests/plupit.spec.ts
@@ -1,21 +1,33 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Pulpi tests', () => {
+test.describe('Pulpit tests', () => {
   test('quick payment with correct data', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/');
-    await page.getByTestId('login-input').fill('testerLO');
-    await page.getByTestId('password-input').fill('password');
+    // Arrange
+    const url = 'https://demo-bank.vercel.app/';
+    const userId = 'testerLO';
+    const userPassword = '10987654';
+
+    const receiverId = '2';
+    const transferAmount = '150';
+    const transferTitle = 'pizza';
+    const expectedTransferReceiver = 'Chuck Demobankowy';
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userId);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await page.locator('#widget_1_transfer_receiver').selectOption('2');
-    await page.locator('#widget_1_transfer_amount').fill('150');
-    await page.locator('#widget_1_transfer_title').fill('pizza');
+    await page.locator('#widget_1_transfer_receiver').selectOption(receiverId);
+    await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+    await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
     await page.getByRole('button', { name: 'wykonaj' }).click();
     await page.getByTestId('close-button').click();
 
+    // Assert
     await expect(page.locator('#show_messages')).toHaveText(
-      'Przelew wykonany! Chuck Demobankowy - 150,00PLN - pizza'
+      `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`
     );
   });
 
@@ -25,14 +37,14 @@ test.describe('Pulpi tests', () => {
     await page.getByTestId('password-input').fill('password');
     await page.getByTestId('login-button').click();
 
-    await page.locator('#widget_1_topup_receiver').selectOption('503 xxx xxx');
+    await page.locator('#widget_1_topup_receiver').selectOption('500 xxx xxx');
     await page.locator('#widget_1_topup_amount').fill('50');
     await page.locator('#uniform-widget_1_topup_agreement span').click();
     await page.getByRole('button', { name: 'doładuj telefon' }).click();
     await page.getByTestId('close-button').click();
 
     await expect(page.locator('#show_messages')).toHaveText(
-      'Doładowanie wykonane! 50,00PLN na numer 503 xxx xxx'
+      'Doładowanie wykonane! 50,00PLN na numer 500 xxx xxx'
     );
   });
 });
