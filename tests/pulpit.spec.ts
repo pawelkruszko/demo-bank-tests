2db950bdc7a41c85ed1fe696ddb8aebbc54765b4
pawelkruszko <pawelkruszko@gmail.com>
2023-07-27 08:28:30 +0200

    run script added

--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -49,4 +49,22 @@ test.describe('Pulpit tests', () => {
     // Assert
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
+
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    // Arrange
+    const topUpReceiver = '500 xxx xxx';
+    const topUpAmount = '50';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topUpAmount);
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpReceiver);
+    await page.locator('#widget_1_topup_amount').fill(topUpAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+  });
 });
